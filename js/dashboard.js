4d7faf3faadc2b39b2bb2bebdd6e1c4f7c7020fd
SPMuto <salwaimy@gmail.com>
2026-08-01 18:15:33 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -11,4 +11,34 @@ async function logout(){
 
     window.location.href = "index.html";
 
-}/ SPMDL
+}
+// Semak pengguna login
+
+async function checkUser(){
+
+    const { data, error } = await supabaseClient.auth.getSession();
+
+    if(!data.session){
+        window.location.href = "index.html";
+    }
+
+    else{
+        document.getElementById("userEmail").innerHTML =
+        data.session.user.email;
+    }
+
+}
+
+
+// Logout
+
+async function logout(){
+
+    await supabaseClient.auth.signOut();
+
+    window.location.href = "index.html";
+
+}
+
+
+checkUser();
