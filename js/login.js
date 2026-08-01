19968b7336fdfd284cc63bf822e37f57d66faf5a
SPMuto <salwaimy@gmail.com>
2026-08-01 17:48:59 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ async function login() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const { data, error } = await supabase.auth.signInWithPassword({
+    const { data, error } = await supabaseClient.auth.signInWithPassword({
         email: email,
         password: password
     });
