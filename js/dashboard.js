25fba424898ccf8cba769760592dbab1a93efc9d
SPMuto <salwaimy@gmail.com>
2026-08-01 18:19:24 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,17 +1,3 @@
-/const { data } = await supabaseClient.auth.getSession();
-
-if (!data.session) {
-    window.location.href = "index.html";
-}
-
-
-async function logout(){
-
-    await supabaseClient.auth.signOut();
-
-    window.location.href = "index.html";
-
-}
 // Semak pengguna login
 
 async function checkUser(){
@@ -19,18 +5,20 @@ async function checkUser(){
     const { data, error } = await supabaseClient.auth.getSession();
 
     if(!data.session){
-        window.location.href = "index.html";
-    }
 
-    else{
+        window.location.href = "index.html";
+
+    } else {
+
         document.getElementById("userEmail").innerHTML =
         data.session.user.email;
+
     }
 
 }
 
 
-// Logout
+// Logout pengguna
 
 async function logout(){
 
@@ -40,5 +28,7 @@ async function logout(){
 
 }
 
+
+// Jalankan semakan
 
 checkUser();
