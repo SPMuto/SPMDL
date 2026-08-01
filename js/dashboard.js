8096956795713bde682102e040f3b7e38cab35bc
SPMuto <salwaimy@gmail.com>
2026-08-01 18:11:46 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,14 @@
-// SPMDL
+/const { data } = await supabaseClient.auth.getSession();
+
+if (!data.session) {
+    window.location.href = "index.html";
+}
+
+
+async function logout(){
+
+    await supabaseClient.auth.signOut();
+
+    window.location.href = "index.html";
+
+}/ SPMDL
