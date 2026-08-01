e045f3b545e86c9f0740f4e447d8ff200cb27576
SPMuto <salwaimy@gmail.com>
2026-08-01 17:24:08 +0800

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,17 @@
+async function login() {
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password
+    });
+
+    if (error) {
+        alert("Login gagal: " + error.message);
+    } else {
+        alert("Login berjaya!");
+        window.location.href = "dashboard.html";
+    }
+}
