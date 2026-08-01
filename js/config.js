a305fb35a3f41c3fe2f745581727e18509403fe5
SPMuto <salwaimy@gmail.com>
2026-08-01 17:51:29 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const SUPABASE_URL = "https://ooemyrjfpsmehtbfionr.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_KeHDgndyqbZlxUZ5U30nkw_OPhl2NG6";
 
-const supabaseClient = supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
