0a5491cf4f64db9da8602003779b8906d1576c7f
SPMuto <salwaimy@gmail.com>
2026-08-02 16:30:02 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,29 @@
-// SPMDL
+async function checkUser(){
+
+    const {data}=await supabaseClient.auth.getSession();
+
+    if(!data.session){
+
+        window.location="login.html";
+
+        return;
+
+    }
+
+    const user=document.getElementById("userEmail");
+
+    if(user){
+
+        user.innerHTML=data.session.user.email;
+
+    }
+
+}
+
+async function logout(){
+
+    await supabaseClient.auth.signOut();
+
+    window.location="login.html";
+
+}
